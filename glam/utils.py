48589e419c8ec7780e92496063e402b7754d47c5
Felix Molter (Mac Pro) <moltaire1@gmail.com>
2018-02-21 12:47:02 +0100

    first slow version of prediction, including new utils to extract design and estimates. prediction functional for hierarchical model

--- glam/utils.py
+++ glam/utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import numpy as np
+import pandas as pd
 from scipy.stats import mode
 
 
@@ -81,3 +82,92 @@ def extract_modes(traces, parameters=None, precision=None, f_burn=0.5):
         return modes[0]
     else:
         return modes
+
+
+def get_design(model):
+    """
+    Extract information about the experimental design
+    from `model.data` and `model.depends_on`.
+    This information is used to map parameter estimates
+    back to subjects, etc.
+
+    Parameters:
+    model: GLAM.model
+
+    Returns:
+    dict
+    """
+    parameters = ['v', 'gamma', 's', 'tau', 't0']
+
+    subjects = model.data['subject'].unique()
+
+    design = dict()
+    subject_parameter_mapping = dict()  # this contains index: ID mappings for every parameter condition
+
+    for parameter in parameters:
+        dependence = model.depends_on.get(parameter)
+
+        if dependence is not None:
+            conditions = model.data[dependence].unique()
+            m = np.zeros((subjects.size, conditions.size))  # empty design matrix for this parameter
+            # create parameter_mapping containing condition names and indices (i.e., columns of design matrix)
+            design[parameter + '_mapping'] = {condition: c
+                                              for c, condition in enumerate(conditions)}
+            design[parameter + '_conditions'] = conditions
+            for c, condition in enumerate(conditions):
+                data_subset = model.data[model.data[dependence] == condition].copy()
+                # find all subject_IDs in this condition
+                subject_subset = data_subset['subject'].unique()
+                # within this condition, generate mapping between subject_IDs and indices
+                subject_parameter_mapping[parameter + '_' + condition] = {s: int(subject)
+                                                                          for s, subject in enumerate(subject_subset)}
+                # Set cells with subject in this condition to 1
+                for s, subject in enumerate(subject_subset):
+                    m[subject_parameter_mapping[parameter + '_' + condition][s], c] = 1
+        else:
+            m = np.ones((subjects.size, 1))
+
+        design[parameter] = m
+        design['subject_parameter_mapping'] = subject_parameter_mapping
+
+    return design
+
+
+def map_individual_estimates(model):
+    """
+    Generate a DataFrame containing estimates,
+    with one row per participant and one column
+    per parameter. Note: If a parameter has multiple
+    conditions, multiple columns are added (e.g.,
+    v_slow, v_high).
+
+    Parameters:
+    model: GLAM.model
+
+    Returns:
+    DataFrame
+    """
+    design = get_design(model)
+    subjects = model.data['subject'].unique()
+    parameters = ['v', 'gamma', 's', 'tau', 't0']
+
+    estimate_df = pd.DataFrame(dict(subject=subjects))
+
+    for parameter in parameters:
+        dependence = model.depends_on.get(parameter, None)
+        if dependence is not None:
+            conditions = model.data[dependence].unique()
+            for condition in conditions:
+                par_con = parameter + '_' + condition
+                condition_index = design[parameter + '_mapping'][condition]
+                subject_map = design['subject_parameter_mapping'][par_con]
+                estimate_df[par_con] = np.zeros(subjects.size) * np.nan
+                for i, estimate in enumerate(model.estimates[par_con]):
+                    estimate_df[par_con][estimate_df['subject'] == subject_map[i]] = estimate
+
+        else:
+            estimate_df[parameter] = np.zeros(subjects.size) * np.nan
+            for i, estimate in enumerate(model.estimates[parameter]):
+                estimate_df[parameter][estimate_df['subject'] == i] = estimate
+
+    return estimate_df
