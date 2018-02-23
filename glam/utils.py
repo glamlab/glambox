8ca82eb308f649ad95b901e3b265e3e64ece80c4
Felix Molter (Mac Pro) <moltaire1@gmail.com>
2018-02-23 09:40:26 +0100

    pep8 housekeeping

--- glam/utils.py
+++ glam/utils.py
@@ -104,18 +104,18 @@ def get_design(data, depends_on):
     subjects = subject_idx.unique()
 
     design = dict()
-    
+
     for parameter in parameters:
         design[parameter] = dict()
         # adding an index defining which entry in data belongs to which subject
         design[parameter]['subject_index'] = subject_idx[:].astype(np.int)
-        
+
         dependence = depends_on.get(parameter)
         if dependence is not None:
             # extract condition levels
             conditions = data[dependence].unique()
             design[parameter]['conditions'] = conditions
-            
+
             # Initialize empty design matrix D for this parameter
             D = np.zeros((subjects.size, conditions.size), dtype=np.int)
             # create parameter mapping containing condition names and indices (i.e., columns of design matrix)
@@ -126,14 +126,17 @@ def get_design(data, depends_on):
 
             # For each condition level
             for c, condition in enumerate(conditions):
-                # mark which trial-entries belong to this condition
-                design[parameter]['condition_index'][data[dependence]==condition] = c
-
                 design[parameter][condition] = dict()
+
+                # mark which trial-entries belong to this condition
+                design[parameter]['condition_index'][data[dependence] == condition] = c
+
                 # Subset data to condition-specific data
-                data_subset = data[data[dependence]==condition].copy()
+                data_subset = data[data[dependence] == condition].copy()
+
                 # find all subject_IDs in this condition
                 subject_subset = data_subset['subject'].unique()
+
                 # within this level, generate mapping between subject_ids and indices
                 # e.g., gamma_high[5] corresponds to subject_id 10
                 design[parameter][condition]['subject_mapping'] = {s: int(subject)
@@ -142,7 +145,7 @@ def get_design(data, depends_on):
                 for s, subject in enumerate(subject_subset):
                     D[np.int(subject), c] = np.int(s+1)
         else:
-            D = (np.arange(subjects.size)[:,None] + 1).astype(np.int)
+            D = (np.arange(subjects.size)[:, None] + 1).astype(np.int)
             design[parameter]['conditions'] = None
             design[parameter]['condition_index'] = np.zeros_like(subject_idx, dtype=np.int)
 
@@ -152,9 +155,9 @@ def get_design(data, depends_on):
         # Detect within / between subject design
         if D.shape[1] == 1:
             design_type = 'fixed'
-        elif np.all((D!=0).sum(axis=1) > 1):
+        elif np.all((D != 0).sum(axis=1) > 1):
             design_type = 'within'
-        elif np.all((D!=0).sum(axis=1) == 1):
+        elif np.all((D != 0).sum(axis=1) == 1):
             design_type = 'between'
         else:
             design_type = 'mixed'
