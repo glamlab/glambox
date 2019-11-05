075843691a5ab8bb064ed3ca51da2318db49fb96
Felix Molter (MBPr) <moltaire1@gmail.com>
2019-11-05 17:28:36 +0100

    bugfix

--- glambox/analysis/__init__.py
+++ glambox/analysis/__init__.py
@@ -1,8 +1,8 @@
 from .analysis import aggregate_subject_level_data, aggregate_group_level_data, compare_parameters, compare_models
 
 __all__ = [
-    aggregate_subject_level_data,
-    aggregate_group_level_data,
-    compare_parameters,
-    compare_models
+    'aggregate_subject_level_data',
+    'aggregate_group_level_data',
+    'compare_parameters',
+    'compare_models'
 ]
