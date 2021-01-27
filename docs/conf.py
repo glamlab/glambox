ccec729078f9aa533ac8e16b010e8d9998343d80
Felix Molter <moltaire1@gmail.com>
2021-01-27 19:20:25 +0100

    try this

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -76,3 +75,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# https://stackoverflow.com/a/56448499
+master_doc = 'index'
